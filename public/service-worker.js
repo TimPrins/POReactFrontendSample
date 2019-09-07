24cc1f2461deadb8c39d10861b02354d9af413a6
Jason <jason@workboost.io>
2019-09-07 17:51:31 +0200

    alvast service worker file toegevoegd

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+//hier komt de serivce worker, maar kan ik pas doen nadat state persistent is.
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
